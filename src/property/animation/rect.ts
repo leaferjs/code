01788e2a55edcd877e7a52a5e507bad1f2d05469
leaferjs <leaferjs@163.com>
2024-09-11 10:46:31 +0800

    add animation

---
+++ src/property/animation/rect.ts
@@ -0,0 +1,18 @@
+import { Leafer, Rect } from 'leafer-ui'
+import '@leafer-in/animate'
+
+const leafer = new Leafer({ view: window })
+
+const rect = new Rect({
+    y: 100,
+    cornerRadius: 50,
+    fill: '#32cd79',
+    animation: { // [!code hl:6]
+        style: { x: 500, cornerRadius: 0 }, // style keyframe
+        // options
+        duration: 1,
+        direction: 'alternate'
+    }
+})
+
+leafer.add(rect)
