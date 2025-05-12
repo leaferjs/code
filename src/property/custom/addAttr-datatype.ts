1db8efc2c970b0342fe790725b53af77a78f9593
leaferjs <leaferjs@163.com>
2025-05-12 10:47:05 +0800

    v1.6.2

--- src/property/custom/addAttr-datatype.ts
+++ src/property/custom/addAttr-datatype.ts
@@ -3,7 +3,7 @@ import { Leafer, Text, dataType } from 'leafer-ui'
 
 const leafer = new Leafer({ view: window })
 
-Text.addAttr('version', '1.6.1', dataType)  //  [!code hl]
+Text.addAttr('version', '1.6.2', dataType)  //  [!code hl]
 
 // default version
 
@@ -11,10 +11,10 @@ const text = new Text({ text: 'Welcome to LeaferJS' })
 
 leafer.add(text)
 
-console.log((text as any).version) // 1.6.1
+console.log((text as any).version) // 1.6.2
 
 // set version
 
-const text2 = new Text({ version: '1.6.1' } as any)
+const text2 = new Text({ version: '1.6.2' } as any)
 
-console.log((text2 as any).version) // 1.6.1
+console.log((text2 as any).version) // 1.6.2
