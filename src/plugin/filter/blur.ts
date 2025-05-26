d010a6a7c4bbd68389b93ad7386dd3de31a22c1e
leaferjs <leaferjs@163.com>
2025-05-26 12:11:01 +0800

    update(filter)

--- src/plugin/filter/blur.ts
+++ src/plugin/filter/blur.ts
@@ -7,6 +7,7 @@ Filter.register('blur', {
     apply(filter, _ui, worldBounds, currentCanvas, _orginCanvas, _shape) { // 应用自定义滤镜
         currentCanvas.filter = `blur(${filter.blur}px)`
         currentCanvas.resetTransform()
+        // currentCanvas.clipWorld(worldBounds) // 某些svg滤镜需要裁剪区域再应用，否则会污染画布
         currentCanvas.copyWorld(currentCanvas, worldBounds, worldBounds, "copy")
         currentCanvas.filter = 'none'
     },
