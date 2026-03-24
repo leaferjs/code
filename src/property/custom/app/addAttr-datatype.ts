8fe289a4b3ce9265c772dedbe57acc52ed94e5b6
leaferjs <leaferjs@163.com>
2026-03-24 22:03:04 +0800

    v2.0.4

--- src/property/custom/app/addAttr-datatype.ts
+++ src/property/custom/app/addAttr-datatype.ts
@@ -5,7 +5,7 @@ import '@leafer-in/viewport' // 导入视口插件 (可选)
 
 const app = new App({ view: window, editor: {} })
 
-Text.addAttr('version', '2.0.3', dataType)  //  [!code hl]
+Text.addAttr('version', '2.0.5', dataType)  //  [!code hl]
 
 // default version
 
@@ -13,10 +13,10 @@ const text = new Text({ text: 'Welcome to AppJS' })
 
 app.tree.add(text)
 
-console.log((text as any).version) // 2.0.3
+console.log((text as any).version) // 2.0.5
 
 // set version
 
-const text2 = new Text({ version: '2.0.3' } as any)
+const text2 = new Text({ version: '2.0.5' } as any)
 
-console.log((text2 as any).version) // 2.0.3
+console.log((text2 as any).version) // 2.0.5
